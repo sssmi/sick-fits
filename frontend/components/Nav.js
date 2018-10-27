aae25e9b280e983b99c24526a6f580ec67d0cffb
sssmi <simeon.kerkola@gmail.com>
2018-10-27 21:56:24 +0300

    Add Header and Meta components

---
+++ frontend/components/Nav.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Nav = () => {
+  return (
+    <div>
+      <Link href="/sell">
+        <a>sell</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Nav;
