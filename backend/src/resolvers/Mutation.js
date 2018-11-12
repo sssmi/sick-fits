da96c48fdee1ce035e099c533c0962b47925f488
sssmi <simeon.kerkola@gmail.com>
2018-11-12 21:46:58 +0200

    Create new item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,22 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    console.log(args);
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
   },
+
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   console.log(args);
+  // },
 };
 
 module.exports = Mutations;
