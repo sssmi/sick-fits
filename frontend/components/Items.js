b0a61f0aa7857df49209042fb82ac9da87ed6f41
sssmi <simeon.kerkola@gmail.com>
2018-11-25 21:31:29 +0200

    Add ItemsList and Items to the page

---
+++ frontend/components/Items.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import styled from 'styled-components';
+import Item from './Item';
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+`;
+
+class Items extends React.Component {
+  render() {
+    return (
+      <Center>
+        <p>Items!</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>loading</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item key={item.id} item={item} />
+                ))}
+              </ItemsList>
+            );
+          }}
+        </Query>
+      </Center>
+    );
+  }
+}
+
+export default Items;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Items from '../components/Items';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <p>Hey!</p>
+        <Items />
       </div>
     );
   }
