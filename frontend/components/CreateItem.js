3552e5733dda0ac3f78fcc4eb1671fb194550915
sssmi <simeon.kerkola@gmail.com>
2018-12-09 12:21:06 +0200

    Upload image

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import Router from 'next/router';
 import gql from 'graphql-tag';
@@ -26,90 +26,122 @@ const CREATE_ITEM_MUTATION = gql`
   }
 `;
 
-function CreateItem(props) {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
-  const [largeImage, setLargeImage] = useState('');
-  const [price, setPrice] = useState(0);
+class CreateItem extends Component {
+  state = {
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
+  };
 
-  const handleChange = (e) => {
+  handleChange = (e) => {
     const { name, value, type } = e.target;
     const val = type === 'number' ? parseFloat(value, 10) : value;
 
-    if (name === 'title') setTitle(val);
-    else if (name === 'description') setDescription(val);
-    else setPrice(val);
+    this.setState({ [name]: val });
   };
-  return (
-    <Mutation
-      mutation={CREATE_ITEM_MUTATION}
-      variables={{
-        title,
-        description,
-        image,
-        largeImage,
-        price,
-      }}
-    >
-      {(createItem, { loading, error }) => (
-        <Form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            const res = await createItem();
-            console.log('res', res);
-            Router.push({
-              pathname: '/item',
-              query: { id: res.data.createItem.id },
-            });
-          }}
-        >
-          <ErrorMessage error={error} />
-          <fieldset disabled={loading} aria-busy={loading}>
-            <label htmlFor="title">
-              Title
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Title"
-                value={title}
-                onChange={handleChange}
-                required
-              />
-            </label>
 
-            <label htmlFor="price">
-              Price
-              <input
-                type="number"
-                id="price"
-                name="price"
-                placeholder="Price"
-                value={price}
-                onChange={handleChange}
-                required
-              />
-            </label>
+  uploadFile = async (e) => {
+    console.log('uploading file');
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+    const res = await fetch('https://api.cloudinary.com/v1_1/d567uy/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={CREATE_ITEM_MUTATION}
+        variables={{
+          title: this.state.title,
+          description: this.state.description,
+          image: this.state.image,
+          largeImage: this.state.largeImage,
+          price: this.state.price,
+        }}
+      >
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const res = await createItem();
+              console.log('res', res);
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
+              });
+            }}
+          >
+            <ErrorMessage error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload"
+                  onChange={this.uploadFile}
+                  required
+                />
+              </label>
 
-            <label htmlFor="description">
-              Description
-              <textarea
-                type="text"
-                id="description"
-                name="description"
-                placeholder="Enter A Description"
-                value={description}
-                onChange={handleChange}
-                required
-              />
-            </label>
-            <button type="submit">Submit </button>
-          </fieldset>
-        </Form>
-      )}
-    </Mutation>
-  );
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+
+              <label htmlFor="description">
+                Description
+                <textarea
+                  type="text"
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                  required
+                />
+              </label>
+              <button type="submit">Submit </button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    );
+  }
 }
 
 export { CREATE_ITEM_MUTATION };
