69bb575e2e6d84195621218816b6db4dc179d51c
sssmi <simeon.kerkola@gmail.com>
2019-05-27 10:01:00 +0300

    29 - Backend Password Reset Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const createToken = id => {
   return jwt.sign({ userId: id }, process.env.APP_SECRET);
@@ -97,6 +99,66 @@ const Mutations = {
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Goodbye!' };
+  },
+
+  async requestReset(parent, { email }, ctx, info) {
+    // Check if real user
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) throw new Error(`No such user found with email ${email}`);
+
+    // Set reset token expiry
+    // Promisify enables to use randomBytes asynchronously
+    const randomBytesPromisified = promisify(randomBytes);
+
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hour
+    const res = await ctx.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    // console.log('res', res);
+    return { message: 'thanks' };
+
+    // Email them the reset token
+  },
+
+  async resetPassword(parent, { password, confirmPassword, resetToken }, ctx, info) {
+    // Check if passwords match
+    if (password !== confirmPassword) throw new Error(`Passwords don't match`);
+
+    // Check if token is expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetTokenExpiry_gte: Date.now() - 3600000,
+        resetToken,
+      },
+    });
+    if (!user) throw new Error(`Invalid or expired reset token`);
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save the new password and remove reset token
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password: hashedPassword,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+    // Generate JWT
+    const token = createToken(updatedUser.id);
+
+    // Set JWT Cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+
+    // Return the new user
+    return updatedUser;
   },
 
   // createDog(parent, args, ctx, info) {
